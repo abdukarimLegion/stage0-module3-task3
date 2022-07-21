08ea8089b1976f73c21476f267d5bcdc6b537c44
Abdukarim <abdukarimuz@mail.ru>
2022-07-21 11:21:33 +0500

    finished

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -3,5 +3,10 @@ package lang.print.gaps.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int fisrt = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.println(fisrt +"\n"+second+"\n"+ third);
     }
 }
